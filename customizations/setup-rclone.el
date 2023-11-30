2e5e1cf14ee90bdb8578799cbf9dcc71e05eab3b
Nitin Anand <gmail@an.andnit.in>
2023-11-30 16:16:06 +0000

    Fixed rclone

--- customizations/setup-rclone.el
+++ customizations/setup-rclone.el
@@ -9,5 +9,5 @@
   "Sync data to dropbox"
   (interactive)
   (if (and (boundp 'rclone-remote-dir) (boundp 'rclone-local-dir))
-      (shell-command (concat "rclone sync " rclone-remote-dir " " rclone-local-dir))
+      (shell-command (concat "rclone sync " rclone-local-dir " " rclone-remote-dir))
     (error "rclone-remote-dir or rclone-local-dir not defined")))
