15ba73c92004998739813f8bd10dd1678a6c1e58
Nitin Anand <gmail@an.andnit.in>
2023-12-02 23:05:08 +0530

    Allow rclone bisync

--- customizations/setup-rclone.el
+++ customizations/setup-rclone.el
@@ -11,3 +11,11 @@
   (if (and (boundp 'rclone-remote-dir) (boundp 'rclone-local-dir))
       (shell-command (concat "rclone sync " rclone-local-dir " " rclone-remote-dir))
     (error "rclone-remote-dir or rclone-local-dir not defined")))
+
+(defun rclone-sync()
+  "Sync data with dropbox"
+  (interactive)
+  (if (and (boundp 'rclone-remote-dir) (boundp 'rclone-local-dir))
+      (if (file-exists-p "~/.cache/rclone/bisync")
+          (shell-command (concat "rclone bisync " rclone-local-dir " " rclone-remote-dir))
+        (shell-command (concat "rclone bisync --resync " rclone-local-dir " " rclone-remote-dir)))))
